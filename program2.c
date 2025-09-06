019bbbdb1fa883e6633449ebcdc29f87644a648b
DakshVegad <dakshvegad2@gmail.com>
2025-09-06 21:47:51 +0530

    Add files via upload

---
+++ program2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int length=70 ;
+    int breadth=90;
+    
+
+    printf("the area of the rectangular plot is %d meter squre \n",length*breadth);
+    printf("the perameter of the rectangular plot is %d meters\n",2*(length+breadth));
+    return 0;
+}
